3a1eb172ba5a17ad1e668b0be24b1ad9b20e849b
Nika Beriddze <rexoman65@gmail.com>
2026-02-02 12:45:01 +0400

    remove: custom JwtDecoder bean creator (Get from access token)

--- product-service/src/main/java/com/kukusha/product_service/config/JwtConfig.java
+++ product-service/src/main/java/com/kukusha/product_service/config/JwtConfig.java
@@ -1,38 +1,21 @@
 package com.kukusha.product_service.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.kukusha.token_service.model.TokenType;
+import com.kukusha.token_service.service.TokenProcessorDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-
-import java.security.KeyFactory;
-import java.security.interfaces.RSAPublicKey;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
 
 @Configuration
 public class JwtConfig {
-    @Value("${token.public-key}")
-    private String publicKeyPEM;
+    private final TokenProcessorDriver tokenProcessorDriver;
+
+    public JwtConfig(TokenProcessorDriver tokenProcessorDriver) {
+        this.tokenProcessorDriver = tokenProcessorDriver;
+    }
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        try {
-            // Parse public key
-            String publicKeyContent = publicKeyPEM
-                    .replace("-----BEGIN PUBLIC KEY-----", "")
-                    .replace("-----END PUBLIC KEY-----", "")
-                    .replaceAll("\\s", "");
-
-            byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyContent);
-            X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            RSAPublicKey publicKey = (RSAPublicKey) keyFactory.generatePublic(publicKeySpec);
-
-            return NimbusJwtDecoder.withPublicKey(publicKey).build();
-        } catch (Exception e) {
-            throw new IllegalStateException("Failed to load RSA public key", e);
-        }
+        return tokenProcessorDriver.getTokenProcessor(TokenType.ACCESS).encObj().getJwtDecoder();
     }
 }
