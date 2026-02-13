455d462f373d808eee93ad59476c484c940e66db
Nika Beriddze <rexoman65@gmail.com>
2026-02-13 11:31:22 +0400

    add: whenComplete in send message

--- kafka-messages-sender-api/src/main/java/com/kukusha/kafka_messages_sender/api/KafkaMessagesSenderAPI.java
+++ kafka-messages-sender-api/src/main/java/com/kukusha/kafka_messages_sender/api/KafkaMessagesSenderAPI.java
@@ -2,10 +2,12 @@ package com.kukusha.kafka_messages_sender.api;
 
 import com.kukusha.kafka_messages_sender.model.KafkaMessageObject;
 import com.kukusha.kafka_messages_sender.model.KafkaMessagesTopics;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class KafkaMessagesSenderAPI {
     private final KafkaTemplate<String, KafkaMessageObject> kafkaTemplate;
 
@@ -15,6 +17,14 @@ public class KafkaMessagesSenderAPI {
 
     public void sendMessage(KafkaMessagesTopics topics,
                             KafkaMessageObject object) {
-        kafkaTemplate.send(topics.getKey(), object.getUsername(), object);
+        String key = topics.getKey();
+        kafkaTemplate.send(key, object.getUsername(), object)
+                .whenComplete((result, error) -> {
+                    if (error != null) {
+                        log.error("Error while sending kafka messages to topics {}", key, error);
+                    } else {
+                        log.info("Kafka messages sent to topics {}", key);
+                    }
+                });
     }
 }
