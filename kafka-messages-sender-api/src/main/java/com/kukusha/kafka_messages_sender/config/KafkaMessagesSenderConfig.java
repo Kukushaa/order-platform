a762a9c6839fed8c17da64c01cfb607bc3c7f8c9
Nika Beriddze <rexoman65@gmail.com>
2026-02-13 11:29:22 +0400

    remove: PropertySource from KafkaMessagesSenderConfig, JsonCreator from RegisterSuccessfullEmailData

--- kafka-messages-sender-api/src/main/java/com/kukusha/kafka_messages_sender/config/KafkaMessagesSenderConfig.java
+++ kafka-messages-sender-api/src/main/java/com/kukusha/kafka_messages_sender/config/KafkaMessagesSenderConfig.java
@@ -2,10 +2,8 @@ package com.kukusha.kafka_messages_sender.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("com.kukusha.kafka_messages_sender")
-@PropertySource("classpath:application.yml")
 public class KafkaMessagesSenderConfig {
 }
--- kafka-messages-sender-api/src/main/java/com/kukusha/kafka_messages_sender/model/RegisterSuccessfullEmailData.java
+++ kafka-messages-sender-api/src/main/java/com/kukusha/kafka_messages_sender/model/RegisterSuccessfullEmailData.java
@@ -1,12 +1,10 @@
 package com.kukusha.kafka_messages_sender.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class RegisterSuccessfullEmailData extends KafkaEmailMessageObject {
-    @JsonCreator
     public RegisterSuccessfullEmailData(@JsonProperty("email") String email,
                                         @JsonProperty("username") String username) {
         super(username, email);
