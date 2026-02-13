d9f0aa35cafb855274f4eb435a096570caf59ff1
Nika Beriddze <rexoman65@gmail.com>
2026-02-13 16:14:44 +0400

    Add caches in orders-service

--- orders-service/src/main/java/com/kukusha/orders_service/database/service/OrdersService.java
+++ orders-service/src/main/java/com/kukusha/orders_service/database/service/OrdersService.java
@@ -1,8 +1,9 @@
 package com.kukusha.orders_service.database.service;
 
 import com.kukusha.orders_service.database.model.Order;
-import com.kukusha.orders_service.database.repository.OrderHistoryRepository;
 import com.kukusha.orders_service.database.repository.OrdersRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -10,17 +11,17 @@ import java.util.Optional;
 @Service
 public class OrdersService {
     private final OrdersRepository repository;
-    private final OrderHistoryRepository historyRepository;
 
-    public OrdersService(OrdersRepository repository, OrderHistoryRepository historyRepository) {
+    public OrdersService(OrdersRepository repository) {
         this.repository = repository;
-        this.historyRepository = historyRepository;
     }
 
+    @Cacheable(value = "order", key = "#id", unless = "#result == null || #result.isEmpty()")
     public Optional<Order> findById(Long id) {
         return repository.findById(id);
     }
 
+    @CacheEvict(value = "order", key = "#order.id")
     public void save(Order order) {
         repository.save(order);
     }
